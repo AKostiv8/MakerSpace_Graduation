4e11a1087e5c4efc920e522a8c09077cb0338e37
AKostiv8 <nolik2000@yahoo.com>
2018-02-24 22:20:20 +0200

    server + db + angular

---
+++ config/database.js
@@ -0,0 +1,12 @@
+/**
+ * Created by MakerSpace on 24.02.2018.
+ */
+
+module.exports = {
+    apiKey: "AIzaSyBGwg5hAT4ABbppZbkKpWdl36l0HFZ3gC8",
+    authDomain: "makerspace-b61c0.firebaseapp.com",
+    databaseURL: "https://makerspace-b61c0.firebaseio.com",
+    projectId: "makerspace-b61c0",
+    storageBucket: "makerspace-b61c0.appspot.com",
+    messagingSenderId: "161463253412"
+}
--- server.js
+++ server.js
@@ -4,9 +4,25 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
+const cors = require('cors');
+const firebase = require('firebase');
+const config = require('./config/database');
+firebase.initializeApp(config);
 
-app.get('/', (req, res) => {
+
+// Frontend directory
+
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
+
+app.use(express.static(__dirname + '/frontend/dist/'));
+
+// Connection to Angular 2 (index.html)
+app.get('*', (req, res) => {
     res.send('Works');
+   // res.sendFile(path.join(__dirname + '/frontend/dist/index.html'));
 });
 
 app.listen(1111, () => {
